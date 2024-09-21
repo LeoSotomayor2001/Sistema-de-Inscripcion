f87daa9b436b1d5dfe8b5c6ca744175e113833e1
Leodomi <leodomi15@gmail.com>
2024-09-20 21:51:37 -0400

    agregandole animacion al mensaje de bienvenidos

--- src/Layouts/AuthLayout.jsx
+++ src/Layouts/AuthLayout.jsx
@@ -1,12 +1,26 @@
 import { Outlet } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
-
+import { ReactTyped } from "react-typed";
 export const AuthLayout = () => {
     return (
-        <main className='flex items-center justify-center h-screen bg-univercity bg-cover bg-center'>
+        <main className='flex items-center justify-center h-screen bg-univercity bg-cover bg-center flex-col '>
+            <div className="relative p-10 bg-white/30 backdrop-blur-lg border border-white/50 rounded-xl shadow-lg mb-10">
+                <h1 className="text-5xl font-extrabold text-center text-indigo-900 animate-pulse drop-shadow-2xl">
+                    <span className="bg-gradient-to-r from-indigo-800 via-purple-700 to-pink-600 text-transparent bg-clip-text">
+                        <ReactTyped
+                            strings={["Bienvenidos", "Bienvenidas", "Welcome"]}
+                            typeSpeed={60}
+                            backSpeed={60}
+                            loop
+                        />
+                    </span>
+                </h1>
+            </div>
+
+
             <div className='w-full max-w-xl px-4'>
                 <Outlet />
-                <ToastContainer/>
+                <ToastContainer />
             </div>
         </main>
 
